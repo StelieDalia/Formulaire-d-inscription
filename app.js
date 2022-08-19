230c31b17fcafc31e31109f249fb84b821c1c1ef
Stelie Dalia <105008628+StelieDalia@users.noreply.github.com>
2022-08-19 11:03:15 +0100

    modif

--- app.js
+++ app.js
@@ -3,38 +3,49 @@ const formBtnPrev2 = document.querySelector("#btn-2-prev")
 const formBtnNext2 = document.querySelector("#btn-2-next")
 const formBtn3 = document.querySelector("#btn-3")
 
-// Button listener of form 1
+function soumission(){
+
+  event.preventDefault()
+  var nom = document.getElementById("nom").value;
+  var prenom = document.getElementById("prenom").value;
+  var classe = document.getElementById("classe").value;
+  var age = document.getElementById("age").value;
+  var genre = document.getElementById("genre").value;
+
+  const eleve = {
+    Nom : nom,
+    Prenom : prenom,
+    classe : classe, 
+  }
+
 formBtn1.addEventListener("click", function(e) {
     gotoNextForm(formBtn1, formBtnNext2, 1, 2)
     e.preventDefault()
   })
   
-  // Next button listener of form 2
   formBtnNext2.addEventListener("click", function(e) {
     gotoNextForm(formBtnNext2, formBtn3, 2, 3)
     e.preventDefault()
   })
   
-  // Previous button listener of form 2
   formBtnPrev2.addEventListener("click", function(e) {
     gotoNextForm(formBtnNext2, formBtn1, 2, 1)
     e.preventDefault()
   })
   
   const gotoNextForm = (prev, next, stepPrev, stepNext) => {
-    // Get form through the button
     const prevForm = prev.parentElement
     const nextForm = next.parentElement
     const nextStep = document.querySelector(`.step--${stepNext}`)
     const prevStep = document.querySelector(`.step--${stepPrev}`)
-    // Add active/inactive classes to both previous and next form
+    
     nextForm.classList.add("form-active")
     nextForm.classList.add("form-active-animate")
     prevForm.classList.add("form-inactive")
-    // Change the active step element
+    
     prevStep.classList.remove("step-active")
     nextStep.classList.add("step-active")
-    // Remove active/inactive classes to both previous an next form
+  
     setTimeout(() => {
       prevForm.classList.remove("form-active")
       prevForm.classList.remove("form-inactive")
